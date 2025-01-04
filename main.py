86f7d3fd6c3103d5970af0822fb2d668643f992a
Asaf31214 <asaf.kanlipicak@outlook.com>
2025-01-04 06:41:23 +0300

    teams, attack mechanism
    Took 42 minutes

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from dataclasses import dataclass
+
 import pygame
 import asyncio
+
+from queue import Queue
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 600, 600
 
@@ -17,6 +21,16 @@ GRID_HEIGHT = WINDOW_HEIGHT // TILE_SIZE
 x_borders = range(0, WINDOW_WIDTH, TILE_SIZE)
 y_borders = range(0, WINDOW_HEIGHT, TILE_SIZE)
 
+TEAM_COLORS = {"Player1": BLUE, "Player2": RED, "Bot": GREEN}
+
+@dataclass
+class Team:
+    Player1= "Player1"
+    Player2= "Player2"
+    Bot= "Bot"
+
+MAX_HP = 80
+
 
 def get_rect(tile_x: int, tile_y: int, scale: float = 1.0) -> tuple:
     offset_x = TILE_SIZE * (1-scale) / 2
@@ -28,13 +42,32 @@ def get_rect(tile_x: int, tile_y: int, scale: float = 1.0) -> tuple:
 
 
 class Tile:
-    def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
-        self.hp: float = 100
+    def __init__(self, x: int, y: int, team: str = Team.Bot):
+        self._x = x
+        self._y = y
+        self._hp: float = MAX_HP
+        self._team: str = team
 
     def get_coords(self):
-        return self.x, self.y
+        return self._x, self._y
+
+    def get_color(self):
+        return TEAM_COLORS[self._team]
+
+    def get_hp(self):
+        return self._hp
+
+    def _set_hp(self, hp: float):
+        self._hp = max(0.0, min(hp, MAX_HP))
+
+    def switch_team(self, team: str):
+        self._hp = MAX_HP
+        self._team = team
+
+    def receive_attack(self, attacker: "Tile" = None):
+        self._set_hp(self._hp - 10)
+        if self._hp == 0:
+            self.switch_team(attacker._team)
 
 
 class Board:
@@ -69,7 +102,13 @@ async def handle_quit():
 async def handle_click(event: pygame.event, board: Board):
     mouse_x, mouse_y = event.pos
     tile_x, tile_y = mouse_x // TILE_SIZE, mouse_y // TILE_SIZE
-    pass
+    tile = board.get_tile(tile_x, tile_y)
+    click_queue.put(tile)
+    if click_queue.full():
+        attacker= click_queue.get()
+        attacked= click_queue.get()
+        attacked.receive_attack(attacker=attacker)
+
 
 
 # DISPLAY RENDERER
@@ -91,22 +130,28 @@ def draw_tiles(window: pygame.Surface, board: Board):
     for tile_x in range(board.grid_width):
         for tile_y in range(board.grid_height):
             tile = board.get_tile(tile_x, tile_y)
-            scale = tile.hp / 100
+            scale = tile.get_hp() / 100
+
             pygame.draw.rect(
                 surface=window,
-                color=GREEN,
+                color=tile.get_color(),
                 rect=get_rect(tile_x, tile_y, scale),
             )
 
 
 
 running: bool = True
-
+click_queue: Queue[Tile] = Queue(maxsize=2)
 
 async def main():
     pygame.init()
     window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     board = Board(GRID_WIDTH, GRID_HEIGHT)
+
+    player_1_base = Tile(0, 0, Team.Player1)
+    player_2_base = Tile(GRID_WIDTH-1, GRID_HEIGHT-1, Team.Player2)
+    board.set_tile(0, 0, player_1_base)
+    board.set_tile(GRID_WIDTH-1, GRID_HEIGHT-1, player_2_base)
 
     pygame.display.set_caption("Conqueror")
 
