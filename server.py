393895415069e20f6e957dbeddc3ca5013de1452
Asaf31214 <asaf.kanlipicak@outlook.com>
2025-01-05 07:04:59 +0300

    server code cleanup refactor
    Took 7 minutes

--- server.py
+++ server.py
@@ -48,24 +48,12 @@ class Team:
 MAX_HP = 80
 
 
-def get_rect(tile_x: int, tile_y: int, scale: float = 1.0) -> tuple:
-    offset_x = TILE_SIZE * (1 - scale) / 2
-    offset_y = TILE_SIZE * (1 - scale) / 2
-    return (tile_x * TILE_SIZE + offset_x,
-            tile_y * TILE_SIZE + offset_y,
-            TILE_SIZE * scale,
-            TILE_SIZE * scale)
-
-
-def is_adjacent(tile_1: "Tile", tile_2: "Tile", board: "Board") -> bool:
+def is_adjacent(tile_1: "Tile", tile_2: "Tile") -> bool:
     attack_range = 2 if len(board.get_team_tiles(tile_1.get_team())) >= 35 else 1
     tile_1_x, tile_1_y = tile_1.get_coords()
     tile_2_x, tile_2_y = tile_2.get_coords()
     return abs(tile_1_x - tile_2_x) + abs(tile_1_y - tile_2_y) <= attack_range
 
-
-def flip_coin():
-    return random.choice([True, False])
 
 
 class Tile:
@@ -181,7 +169,7 @@ def switch_turn():
     turn = not turn
 
 
-def decide_winner(board: Board, attacker: Tile, attacked: Tile):
+def decide_winner(attacker: Tile, attacked: Tile):
     if attacker.first_attack():
         set_message(f'{attacker} wins the first attack!')
         return True
@@ -217,8 +205,8 @@ def set_message(new_message: str, append: bool = False):
 
 
 # MAIN EVENT HANDLER
-async def event_handler(event: pygame.event, board: Board):
-    await handle_click(event, board)
+async def event_handler(event: pygame.event):
+    await handle_click(event)
 
 
 # Event handler functions
@@ -227,7 +215,7 @@ async def handle_quit():
     running = False
 
 
-async def handle_click(event, board: Board):
+async def handle_click(event):
     if board.get_winner():
         set_message(f'Game over! Winner: {board.get_winner()}')
         return
@@ -246,13 +234,13 @@ async def handle_click(event, board: Board):
             click_queue.clear()
             set_message('Not your turn!')
             return
-        if is_adjacent(attacker, attacked, board):
+        if is_adjacent(attacker, attacked):
             await asyncio.sleep(0.5)
             if attacker.get_team() == attacked.get_team():
                 attacker.swap(attacked)
                 set_message(f'{attacker.get_team()} swapped tiles {attacker} and {attacked}!')
             else:
-                success = decide_winner(board, attacker, attacked)
+                success = decide_winner(attacker, attacked)
                 if not success:
                     attacker, attacked = attacked, attacker
                 attacked.receive_attack(attacker=attacker)
@@ -262,88 +250,6 @@ async def handle_click(event, board: Board):
         click_queue.clear()
 
 
-# DISPLAY RENDERER
-def render(window: pygame.Surface, board: Board):
-    window.fill(WHITE)
-    draw_tiles(window, board)
-    draw_selections(window)
-    draw_lines(window)
-    display_message(window, board)
-    pygame.display.flip()
-
-
-def display_message(window: pygame.Surface, board: Board, font_size: int = 30):
-    font = pygame.font.SysFont('Comic Sans MS', font_size)
-    line_height = font.get_height()
-
-    turn_message = "Your Turn"
-    text_surface = font.render(turn_message, True, BLACK)
-    x_position = 16 if turn else WINDOW_WIDTH - 16 - text_surface.get_width()
-    y_position = WINDOW_HEIGHT + 2 * line_height
-    window.blit(text_surface, (x_position, y_position))
-
-    player_1_score = len(board.get_team_tiles(Team.Player1))
-    player_2_score = len(board.get_team_tiles(Team.Player2))
-    text1_surface = font.render(f"{Team.Player1}: {player_1_score}", True, BLACK)
-    text2_surface = font.render(f"{Team.Player2}: {player_2_score}", True, BLACK)
-    y_position = WINDOW_HEIGHT + line_height
-    x_1_position = 16
-    x_2_position = WINDOW_WIDTH - 16 - text2_surface.get_width()
-    window.blit(text1_surface, (x_1_position, y_position))
-    window.blit(text2_surface, (x_2_position, y_position))
-
-    manual_lines = message.split('\n')
-    lines = []
-    current_line = ""
-
-    for line in manual_lines:
-        lines.append(current_line)
-        current_line = ""
-        words = line.split(' ')
-        for word in words:
-            if font.size(current_line + word + " ")[0] > WINDOW_WIDTH - 20:
-                lines.append(current_line)
-                current_line = word + " "
-            else:
-                current_line += word + " "
-
-    lines.append(current_line)
-
-    for i, line in enumerate(lines):
-        text_surface = font.render(line.strip(), True, BLACK)
-        x_position = (WINDOW_WIDTH - 32) // 2 - text_surface.get_width() // 2
-        y_position = WINDOW_HEIGHT + (i + 4) * line_height
-        window.blit(text_surface, (x_position, y_position))
-
-
-def draw_lines(window: pygame.Surface):
-    for x in x_borders:
-        pygame.draw.line(window, GRID_COLOR, (x, 0), (x, WINDOW_HEIGHT))
-    for y in y_borders:
-        pygame.draw.line(window, GRID_COLOR, (0, y), (WINDOW_WIDTH, y))
-
-
-def draw_tiles(window: pygame.Surface, board: Board):
-    for tile_x in range(board.grid_width):
-        for tile_y in range(board.grid_height):
-            tile = board.get_tile(tile_x, tile_y)
-            scale = tile.get_hp() / 100
-
-            pygame.draw.rect(
-                surface=window,
-                color=tile.get_color(),
-                rect=get_rect(tile_x, tile_y, scale),
-            )
-
-
-def draw_selections(window: pygame.Surface):
-    for tile in click_queue:
-        pygame.draw.rect(
-            surface=window,
-            color=GREEN,
-            rect=get_rect(*tile.get_coords()),
-            width=5
-        )
 
 
 running: bool = True
@@ -362,7 +268,7 @@ def get_events():
     return events
 
 board: Board
-window: pygame.Surface
+
 async def main():
     pygame.init()
     global board
@@ -378,7 +284,7 @@ async def main():
     global running
     while running:
         await asyncio.gather(
-            *[asyncio.create_task(event_handler(event, board))
+            *[asyncio.create_task(event_handler(event))
               for event in get_events()])
         await asyncio.sleep(0.01)  # 100 Tick rate
     pygame.quit()
